d94d7ea5cd0f7050846bf7a532b02068df415453
VladyslavGolovko <86726358+VladyslavGolovko@users.noreply.github.com>
2021-12-24 16:45:15 +0200

    made first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const refs = {
+  body: document.querySelector('body'),
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
+
+let intervalId = null;
+
+function changeColor() {
+  let color = getRandomHexColor();
+  refs.body.style.backgroundColor = color;
+}
+
+function startHandler() {
+  refs.startBtn.disabled = true;
+  intervalId = setInterval(changeColor, 1000);
+}
+
+function stopHandler() {
+  clearInterval(intervalId);
+  refs.startBtn.disabled = false;
+}
+
+refs.startBtn.addEventListener('click', startHandler);
+refs.stopBtn.addEventListener('click', stopHandler);
