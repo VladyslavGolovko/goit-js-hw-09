76438e8aaa7c8a68c58895714245620ec6ec6a78
VladyslavGolovko <86726358+VladyslavGolovko@users.noreply.github.com>
2021-12-27 22:05:20 +0200

    final commit

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,6 +1,79 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+    const data = selectedDates[0];
 
+    choseCorectTime(data);
+  },
+};
+
+const refs = {
+  dateInput: document.querySelector('#datetime-picker'),
+  startBtnEl = document.querySelector('button[data-start]'),
+  days: document.querySelector('span[data-days]'),
+  hours: document.querySelector('span[data-hours]'),
+  minutes: document.querySelector('span[data-minutes]'),
+  seconds: document.querySelector('span[data-seconds]'),
+};
+
+let time = null;
+
+refs.startBtnEl.disabled = true;
+refs.startBtnEl.addEventListener('click', startCount);
+
+function choseCorectTime(data) {
+  time = data;
+  if (data < Date.now()) {
+    Notiflix.Notify.failure('Please choose a date in the future');
+  } else {
+    refs.startBtnEl.disabled = false;
+    return time;
+  }
+};
+
+function startCount() {
+  setInterval(() => {
+    const newTime = Date.now();
+    let timeLeft = time - newTime;
+
+    const result = convertMs(timeLeft);
+    console.log(result);
+
+    refs.days.textContent = addLeadingZero(result.days);
+    refs.hours.textContent = addLeadingZero(result.hours);
+    refs.minutes.textContent = addLeadingZero(result.minutes);
+    refs.seconds.textContent = addLeadingZero(result.seconds);
+  }, 1000)
+};
+
+function addLeadingZero(value) {
+  return value.ToString().padStart(2, '0');
+}
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+/*import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
 const refs = {
   startBtn: document.querySelector('button[data-start'),
   datetimePickerInput: document.querySelector('#datetime-picker'),
@@ -71,4 +144,4 @@ function convertMs(ms) {
   const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
 
   return { days, hours, minutes, seconds };
-}
+}*/
