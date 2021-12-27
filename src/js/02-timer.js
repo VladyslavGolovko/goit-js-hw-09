5d1e461d2f5f1d1421dcdd64ab3056da7b985e15
VladyslavGolovko <86726358+VladyslavGolovko@users.noreply.github.com>
2021-12-27 22:42:19 +0200

    made some changes

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -73,10 +73,10 @@ function convertMs(ms) {
   const hour = minute * 60;
   const day = hour * 24;
 
-  const days = Math.floor(ms / day);
-  const hours = Math.floor((ms % day) / hour);
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  const days = addLeadingZero(Math.floor(ms / day));
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
 
   return { days, hours, minutes, seconds };
 }
