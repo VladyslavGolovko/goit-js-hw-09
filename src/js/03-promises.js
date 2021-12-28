838ee27756616ed7907d544ef58cc92daa255050
VladyslavGolovko <86726358+VladyslavGolovko@users.noreply.github.com>
2021-12-28 18:51:38 +0200

    made 3-d task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,4 +1,48 @@
 import Notiflix from 'notiflix';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+const formEl = document.querySelector('form');
+let position = 0;
+
+formEl.addEventListener('click', onFormSubmit);
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  let delay = Number(e.currentTarget.elements.delay.value);
+  const amount = Number(e.currentTarget.elements.amount.value);
+  const step = Number(e.currentTarget.elements.step.value);
+
+  for (let i = 1; i <= amount; i += 1) {
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`, {
+          width: '40vw',
+        });
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`, {
+          width: '40vw',
+        });
+      });
+    delay += step;
+    position += 1;
+  }
+}
+
+function createPromise(position, delay) {
+  const promise = new Promise((resolve, reject) => {
+    setInterval(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+  return promise;
+}
+
+/*import Notiflix from 'notiflix';
 const refs = {
   form: document.querySelector('form'),
 };
